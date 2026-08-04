8a945d185c43c71fe4c07c5242f9a5adee155412
leoonavas <leoohnavas@gmail.com>
2026-08-04 14:17:16 -0300

    Cristiano Commit

---
+++ Aula-03/Repeticao/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        string senha = "";
+
+        while (senha != "2444")
+        {
+            Console.Write("Digite a sua senha: ");
+            senha = Console.ReadLine();
+
+
+
+        }
+        Console.WriteLine("Acesso Permitido!");
+    }
+}
