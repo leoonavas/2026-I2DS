ee1fbb1e8e4a79c7f54c38c31c470105bbf39758
leoonavas <leoohnavas@gmail.com>
2026-08-04 14:58:56 -0300

    Vetores revisao de vetores

--- Aula-03/Vetores/Program.cs
+++ Aula-03/Vetores/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] listaFrutas = { "Maca", "Manga", "Morango", "Melancia", "Melao" };
+
+        Console.WriteLine("Acessar o terceiro elemento da lista: ");
+        Console.WriteLine(listaFrutas[2]);
+
+        Console.WriteLine("Alterar o segundo elemento da lista: ");
+        listaFrutas[1] = "Banana";
+
+        Console.WriteLine("Alterado com sucesso!");
+
+        Console.WriteLine("\n==============================================\n");
+
+        for (int indice = 0; indice < listaFrutas.Length; indice++)
+        {
+            Console.WriteLine(listaFrutas[indice]);
+        }
+
+        Console.WriteLine("\n==============================================\n");
+
+        foreach (string fruta in listaFrutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+    }
+}
--- Aula-03/Vetores/obj/Debug/net10.0/Vetores.AssemblyInfo.cs
+++ Aula-03/Vetores/obj/Debug/net10.0/Vetores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ceae9442d1a4a1d3fa464649c989bcc7d8256e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92624a22badf03f0023af7daf31d54267e88017a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
