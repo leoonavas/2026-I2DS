4c5a770d4bd299f1d682d33690c9effdb4205fcd
leoonavas <leoohnavas@gmail.com>
2026-08-04 16:52:36 -0300

    Mais exercicios feitos

--- Aula-03/Exercicios/Exercicio-03/Program.cs
+++ Aula-03/Exercicios/Exercicio-03/Program.cs
@@ -21,6 +21,36 @@ internal class Program
     }
     public static void Exercicio03()
     {
-        
+        string nome = "";
+        Console.Write("Digite o seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Olá, " + nome + "! Seja bem-vindo!");
+
+        int nota1;
+        Console.Write("Digite o sua primeira nota: ");
+        nota1 = int.Parse(Console.ReadLine());
+
+        int nota2;
+        Console.Write("Digite o sua segunda nota: ");
+        nota2 = int.Parse(Console.ReadLine());
+
+        int nota3;
+        Console.Write("Digite o sua terceira nota: ");
+        nota3 = int.Parse(Console.ReadLine());
+
+        int media = (nota1 + nota2 + nota3) / 3;
+
+        Console.WriteLine("Sua media final foi " + media + ".");
+        if (media >= 7)
+        {
+            Console.WriteLine("Resultado Final: Aprovado");
+
+        } else
+        {
+            Console.WriteLine("Resultado Final: Reprovado");
+
+        }
+
+
     }
 }
---
+++ Aula-03/Exercicios/Exercicio-04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.Versioning;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Line(4);
+        Exercicio04();
+
+    }
+
+    public static void Line(int num)
+    {
+        Console.WriteLine("\nExercicio " + num + "  ============================================");
+    }
+    public static void Print(string text)
+    {
+        Console.WriteLine(text);
+    }
+    public static void Exercicio04()
+    {
+        int valor;
+
+        Console.Write("Digite o valor do saque: ");
+        valor = int.Parse(Console.ReadLine());
+
+        int[] notas = { 100, 50, 20, 10, 5, 2 };
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            int quantidadenotas = valor / notas[i];
+
+            valor = valor % notas[i];
+
+            if (quantidadenotas > 0)
+            {
+                if (quantidadenotas == 1)
+                {
+                    Console.WriteLine(quantidadenotas + " nota de " + notas[i]);
+
+                }
+                else
+                {
+                    Console.WriteLine(quantidadenotas + " notas de " + notas[i]);
+
+                }
+            }
+        }
+
+
+    }
+}
