ca8a3a3b552dbe90bdd0d180953a2a4cd921f675
leoonavas <leoohnavas@gmail.com>
2026-08-04 07:44:29 -0300

    Atualiza versão informacional do assembly e cache de entradas

--- Aula-02/Projeto-Aula02/obj/Debug/net10.0/Projeto-Aula02.AssemblyInfo.cs
+++ Aula-02/Projeto-Aula02/obj/Debug/net10.0/Projeto-Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e1a315748cf85fc3619ada16f497c6281dca7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a95a50fa3d46b1e98df9f4a7fb5a411e1aff6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
