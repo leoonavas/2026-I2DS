0aaa25d31044031fd04132f07ff448f132013c8b
leoonavas <leoohnavas@gmail.com>
2026-08-05 08:44:19 -0300

    Implementação do exercício 08 com autenticação de usuário e senha

--- Aula-03/Exercicios/Exercicio-08/Program.cs
+++ Aula-03/Exercicios/Exercicio-08/Program.cs
@@ -21,7 +21,37 @@ internal class Program
     }
     public static void Exercicio08()
     {
-       
+        string userCorreto = "admin";
+        string senhaCorreto = "1234";
+        int tentativas = 0;
+        while (true)
+        {
+            string user = "";
+            Console.Write("Digite seu usuario: ");
+            user = Console.ReadLine();
+
+            string senha = "";
+            Console.Write("Digite sua senha: ");
+            senha = Console.ReadLine();
+
+            if (user == userCorreto && senha == senhaCorreto)
+            {
+                Console.WriteLine("Logado com sucesso!");
+                break;
+            }
+            else
+            {
+                tentativas++;
+                Console.WriteLine("Usuario ou senha incorretos");
+                Console.WriteLine("Tentativas restantes: " + (3 - tentativas));
+            }
+
+            if (tentativas == 3)
+            {
+                Console.WriteLine("Voce atingiu o limite de tentativas. Tente novamente mais tarde.");
+                break;
+            }
+        }
 
     }
 }
