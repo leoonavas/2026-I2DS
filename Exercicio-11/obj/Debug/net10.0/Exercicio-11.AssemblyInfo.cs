e1a23c5786102977e8666d97689e4a57cc165cea
leoonavas <leoohnavas@gmail.com>
2026-08-17 11:22:28 -0300

    cmt

--- Exercicio-11/obj/Debug/net10.0/Exercicio-11.AssemblyInfo.cs
+++ Exercicio-11/obj/Debug/net10.0/Exercicio-11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio-11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843a33555ecb642e99eb0c69910e76b072531408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22787275b6c8b2c9a6de514cb36b47b59820ed2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio-11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio-11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
